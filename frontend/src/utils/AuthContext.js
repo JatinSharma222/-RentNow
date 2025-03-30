acbabf195f143122b78c8d866d1dd333c7401222
Jatin Sharma <sharma.jatin2206@gmail.com>
2025-03-30 16:18:18 +0530

    Cors issue solved 5

--- frontend/src/utils/AuthContext.js
+++ frontend/src/utils/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, useEffect, useCallback } from 'react';
+import React, { createContext, useState, useContext, useCallback } from 'react';
 const API_URL = process.env.REACT_APP_API_URL;
 const AuthContext = createContext(null);
 
